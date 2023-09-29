3c62e62ef7fadd49e7a2ffc271355957446e83e1
Jonas Kaad <jonaskaad@gmail.com>
2023-09-29 10:41:56 +0200

    go subscriber

---
+++ src/subscriber/src/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+    fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+    fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+    fmt.Printf("Connect lost: %v", err)
+}
+
+func main() {
+    var broker = "mosquitto"
+    var port = 1883
+    opts := mqtt.NewClientOptions()
+    opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+    opts.SetClientID("go_mqtt_client")
+    opts.SetDefaultPublishHandler(messagePubHandler)
+    opts.OnConnect = connectHandler
+    opts.OnConnectionLost = connectLostHandler
+    client := mqtt.NewClient(opts)
+    if token := client.Connect(); token.Wait() && token.Error() != nil {
+        panic(token.Error())
+  }
+}
+
+func sub(client mqtt.Client) {
+    topic := "topic/lecture05"
+    token := client.Subscribe(topic, 1, nil)
+    token.Wait()
+  	fmt.Printf("Subscribed to topic: %s", topic)
+}
